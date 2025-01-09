1c37484e32c1a03f0be460aacd4258cac47f6f5b
chenmiao <chenmiao.ku@gmail.com>
2025-01-09 15:33:30 +0000

    [feat :tada:]: init repo

---
+++ include/data_structs/sds.h
@@ -0,0 +1,13 @@
+/* SDSLib 2.0 -- A C dynamic strings library
+ *
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2) or the Server Side Public License v1 (SSPLv1).
+ */
+
+#ifndef __REDISCC_DATA_STRUCTURES_SDS_H
+#define __REDISCC_DATA_STRUCTURES_SDS_H
+
+#endif //! __REDISCC_DATA_STRUCTURES_SDS_H
