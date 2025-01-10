e8ed3bab4f96a328c0e8279ec8adf114f0c6d737
chenmiao <chenmiao.ku@gmail.com>
2025-01-10 10:31:41 +0000

    [feat :sparkles:]: create the basic sds struct

--- include/data_structs/sds.h
+++ include/data_structs/sds.h
@@ -10,4 +10,34 @@
 #ifndef __REDISCC_DATA_STRUCTURES_SDS_H
 #define __REDISCC_DATA_STRUCTURES_SDS_H
 
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+template < typename T >
+concept IsUnsignedInteger =
+  std::is_same_v< T, uint8_t > || std::is_same_v< T, uint16_t > ||
+  std::is_same_v< T, uint32_t > || std::is_same_v< T, uint64_t >;
+
+
+/**
+ * @author chenmiao (chenmiao.ku@gmail.com)
+ * @date 2025-01-10
+ * @brief 
+ * 
+ * @tparam SDSHDR_TYPE 
+ */
+template < IsUnsignedInteger SDSHDR_TYPE >
+class [[gnu::packed]] sds_t {
+private:
+  SDSHDR_TYPE                  len_;   ///<
+  SDSHDR_TYPE                  alloc_; ///<
+  uint8_t                      flags_; ///<
+  std::unique_ptr< uint8_t[] > buf_;   ///<
+
+public:
+public:
+  size_t length() const;
+};
+
 #endif //! __REDISCC_DATA_STRUCTURES_SDS_H
