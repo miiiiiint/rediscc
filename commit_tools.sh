9a0749a66ab6e7327150d4894203ebdd05f1cbca
chenmiao <chenmiao.ku@gmail.com>
2025-01-10 07:26:37 +0000

    [bugfix :bug:]: solve the commit tool cannot deal un-support command

--- commit_tools.sh
+++ commit_tools.sh
@@ -2,6 +2,12 @@
 
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 <commit_type> <commit_message>"
+    echo "Commit types: init, impr, perf, remove, bugfix, hotfix, feat, docs"
+    exit 1
+}
+
 # Function to get emoji code based on commit type
 get_emoji_code() {
     case $1 in
@@ -13,15 +19,13 @@ get_emoji_code() {
         "hotfix") echo ":ambulance:" ;;
         "feat") echo ":sparkles:" ;;
         "docs") echo ":memo:" ;;
-        *) echo "" ;;
+        *) usage ;;
     esac
 }
 
 # Check if commit type and message are provided
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <commit_type> <commit_message>"
-    echo "Commit types: init, impr, perf, remove, bugfix, hotfix, feat, docs"
-    exit 1
+    usage
 fi
 
 # Get the emoji code
